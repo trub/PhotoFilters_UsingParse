eba52b4b56b1259fbf63d29f6fbc3bdc93bd4307
matthew weintrub <heytrub@gmail.com>
2015-11-02 16:15:37 -0800

    make image picker function work

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -15,71 +15,67 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-//        let status = PFObject(className: "Status")
-//        status[kStatusTextKey] = "Took this picture on my way to code fellows today. Seattle is awesome."
-//        status["location"] = "Seattle"
-//        status["hastags"] = "#beastMode"
-//        
-//        status.saveInBackgroundWithBlock { (success, error) -> Void in
-//            if success {
-//                print("Success saving to parse. Check parse console.")
-//            }
-//        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func presentImagePickerButton(sender: UIButton) {
+    
+    @IBAction func presentImagePickerButtonSelected(sender: UIButton) {
         print("pressed")
         
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            
+            let alertController = UIAlertController(title: "YOOOOO", message: "plz choose", preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (alert) -> Void in
+                self.presentImagePicker(.Camera)
+                
+            }
+            
+            let photoLibrary = UIAlertAction(title: "Photos", style: UIAlertActionStyle.Default) { (alert) -> Void in
+                self.presentImagePicker(.PhotoLibrary)
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) -> Void in
+                
+                let alertControllerTwo = UIAlertController(title: "Are you sure?", message: "don't go", preferredStyle: UIAlertControllerStyle.Alert)
+                
+                let okAction = UIAlertAction(title: "WHATEVER MAN", style: UIAlertActionStyle.Default, handler: { (alert) -> Void in
+                    print("WHATEVER MAN")
+                })
+                
+                
+                alertControllerTwo.addAction(okAction)
+                self.presentViewController(alertControllerTwo, animated: true, completion: nil)
+            }
+            
+            
+            alertController.addAction(photoLibrary)
+            alertController.addAction(cameraAction)
+            alertController.addAction(cancelAction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+        } else {
+            self.presentImagePicker(.PhotoLibrary)
+            
+        }
+    
+        
+        
+    }
+    
+    // present image picker
+    func presentImagePicker(sourceType: UIImagePickerControllerSourceType) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
-        
-        let alertController = UIAlertController(title: "Source", message: "plz choose", preferredStyle: UIAlertControllerStyle.ActionSheet)
-        
-        let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (alert) -> Void in
-            imagePicker.sourceType = .Camera
-        }
-        
-        let photoLibrary = UIAlertAction(title: "Photos", style: UIAlertActionStyle.Default) { (alert) -> Void in
-            imagePicker.sourceType = .PhotoLibrary
-        }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) -> Void in
-            
-            let alertControllerTwo = UIAlertController(title: "Are you sure?", message: "don't go", preferredStyle: UIAlertControllerStyle.Alert)
-            
-            let okAction = UIAlertAction(title: "ok, cool", style: UIAlertActionStyle.Default, handler: { (alert) -> Void in
-                print("user agrees")
-            })
-            
-        
-            alertControllerTwo.addAction(okAction)
-            self.presentViewController(alertControllerTwo, animated: true, completion: nil)
-        }
-        
-        
-        alertController.addAction(photoLibrary)
-        alertController.addAction(cameraAction)
-        alertController.addAction(cancelAction)
-        
-        self.presentViewController(alertController, animated: true, completion: nil)
-//        
-//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-//            imagePicker.sourceType = .Camera
-//        } else {
-//            imagePicker.sourceType = .PhotoLibrary
-//        }
-        
+        imagePicker.sourceType = sourceType
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     // UIImagePickerController
-    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         self.imageView.image = image
         //dismiss picker
@@ -91,3 +87,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
 }
+
+
+
+
+
+
+
+
+
+// create a class in parse
+
+// Do any additional setup after loading the view, typically from a nib.
+
+//        let status = PFObject(className: "Status")
+//        status[kStatusTextKey] = "Took this picture on my way to code fellows today. Seattle is awesome."
+//        status["location"] = "Seattle"
+//        status["hastags"] = "#beastMode"
+//
+//        status.saveInBackgroundWithBlock { (success, error) -> Void in
+//            if success {
+//                print("Success saving to parse. Check parse console.")
+//            }
+//        }
